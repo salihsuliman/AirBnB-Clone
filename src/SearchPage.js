b62821f8c168c97b10a1db8770eb95ffbfc358ef
salihsuliman <gusicinc@gmail.com>
2020-09-10 15:42:26 +0100

    Finishing search page

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './SearchPage.css'
 import {Button} from "@material-ui/core"
+import SearchResult from './SearchResult.js'
 
 
 function SearchPage() {
@@ -17,6 +18,49 @@ function SearchPage() {
                 <Button variant="outlined">More filters</Button>
 
             </div>
+
+            <SearchResult 
+            img="https://i.insider.com/5b4d679369e6c9ae628b4619?
+            width=1100&format=jpeg&auto=webp"
+            location="Private room in center of London"
+            title="Stay at this spacious Edwardian House"
+            description="1 guest • 1 bedroom • 1 bed • 1.5
+            shared bathrooms • WiFi • Kitchen • Free parking
+            • Washing machine"
+            star={4.21}
+            price="$30 / night"
+            total="$233 total"
+            
+            />
+
+<SearchResult 
+            img="https://i.insider.com/5b4d679369e6c9ae628b4619?
+            width=1100&format=jpeg&auto=webp"
+            location="Private room in center of London"
+            title="Stay at this spacious Edwardian House"
+            description="1 guest • 1 bedroom • 1 bed • 1.5
+            shared bathrooms • WiFi • Kitchen • Free parking
+            • Washing machine"
+            star={4.21}
+            price="$30 / night"
+            total="$233 total"
+            
+            />
+
+<SearchResult 
+            img="https://i.insider.com/5b4d679369e6c9ae628b4619?
+            width=1100&format=jpeg&auto=webp"
+            location="Private room in center of London"
+            title="Stay at this spacious Edwardian House"
+            description="1 guest • 1 bedroom • 1 bed • 1.5
+            shared bathrooms • WiFi • Kitchen • Free parking
+            • Washing machine"
+            star={4.21}
+            price="$30 / night"
+            total="$233 total"
+            
+            />
+
         </div>
     )
 }
