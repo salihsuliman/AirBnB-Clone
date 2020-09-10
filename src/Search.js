74dea6b38e41709e3925f1366a1c07d25631a97e
salihsuliman <gusicinc@gmail.com>
2020-09-10 15:55:50 +0100

    Done

--- src/Search.js
+++ src/Search.js
@@ -5,9 +5,11 @@ import "react-date-range/dist/theme/default.css"
 import { DateRangePicker } from "react-date-range"
 import PeopleIcon from "@material-ui/icons/People"
 import {Button} from "@material-ui/core"
+import {BrowserRouter as Router, Switch, Route, Link, useHistory} from 'react-router-dom'
 
 
 function Search() {
+    const history = useHistory();
 
     const [startDate, setStartDate] = useState
     (new Date());
@@ -35,7 +37,7 @@ function Search() {
                 <input min={0} 
                 defaultValue={2}
                 type="number" />
-                <Button>
+                <Button onClick={() => history.push('/search')}>
                     Search Airbnb
                 </Button>
                 
